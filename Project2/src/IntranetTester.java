2e99eee937e3e9341d4b6ee175b6db2f397723a9
Yerkebulan <erke.zhumagali@gmail.com>
2017-11-30 23:59:49 +0600

    Editing...

---
+++ Project2/src/IntranetTester.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class IntranetTester {
+	
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		Scanner in = new Scanner(System.in);
+		
+		
+		
+		String base, login, password;
+		while(true) {
+			System.out.println("Welcome to KBTU Intranet");
+			User.LogIn();
+		}
+	}
+
+}
