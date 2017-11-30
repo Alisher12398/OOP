18d7b3c7bafee5376d155a6d0fc496e476ce2e83
Alisher <Alisher12398@gmail.com>
2017-11-30 14:24:41 +0600

    Admin 66%

--- Project2/src/Admin.java
+++ Project2/src/Admin.java
@@ -21,7 +21,6 @@ public class Admin extends User {
 		
 		addUser();
 		addUser();
-		addUser();
 		updateUser();
 		
 	}
@@ -33,35 +32,28 @@ public class Admin extends User {
 		
 		int i=0;
 		String name1 = null,name2 = null,name3 = null,name4 = null;
-		String aname1 = null,aname2 = null,aname3 = null,aname4 = null;
 		
-		//while (in.hasNext())
 		for(int u=0; u<5; u++){
-			//String[] line = in.nextLine().split(";");
 			
 			if (i==0) {
-				//name1=line[0];
 				name1=in.nextLine();
 				System.out.println("***" + "New" + " ***");
 				i++;
 			}
 			
 			if (i==1) {
-				//name2=line[0];
 				name2=in.nextLine();
 				System.out.println("***" + "New" + " ***");
 				i++;
 			}
 			
 			if (i==2) {
-				//name3=line[0];
 				name3=in.nextLine();
 				System.out.println("***" + "New" + " ***");
 				i++;
 			}
 			
 			if (i==3) {
-				//name4=line[0];
 				name4=in.nextLine();
 				System.out.println("***" + "New" + " ***");
 				i++;
@@ -72,78 +64,88 @@ public class Admin extends User {
 		userDataList.add(new User(name1,name2,name3,name4));
 		System.out.println("***" + "New user" +  " ***");
 		
-		BufferedReader bf = new BufferedReader(new FileReader("C:\\OOPGit\\OOP\\Project2output.txt"));
+		BufferedReader bf = new BufferedReader(new FileReader("C:\\OOPGit\\OOP\\Project2\\output.txt"));
 		BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\OOPGit\\OOP\\Project2\\output.txt"));
-	
+		
 		 
 		try{
-			 //PrintWriter pw = new PrintWriter(new FileOutputStream(file));
-	          // int datList = userDataList.size();
-	           
-	           //for (User s : userDataList){
-	             		for(int y = 0; y < userDataList.size(); y++) {
-	            	   bw.write(userDataList.get(y).name + " " + userDataList.get(y).surname + " " + userDataList.get(y).id + " " + userDataList.get(y).password);
-	            	   bw.newLine();	             		}
-	            	  
-	           //}
+			WritetoFile();
 	       } catch(IOException e) {}
 		
-		 bf.close();
-		 bw.close();
-		 
-		 
-		 /*
-		Scanner in = new Scanner(System.in);
-		while (in.hasNext()){
-			   String[] line = in.nextLine().split(";");
-			}
-		*/
+		bf.close();
+		bw.close();
+		 		
 	}
 	
+	public static void WritetoFile() throws IOException {
+		
+		BufferedReader bf2 = new BufferedReader(new FileReader("C:\\OOPGit\\OOP\\Project2\\output.txt"));
+		BufferedWriter bw2 = new BufferedWriter(new FileWriter("C:\\OOPGit\\OOP\\Project2\\output.txt"));
+		
+		try {
+			for (int q=0; q<userDataList.size(); q++) {
+				bw2.write(userDataList.get(q).name + " " + userDataList.get(q).surname + " " + userDataList.get(q).id + " " + userDataList.get(q).password);
+	      	  	bw2.newLine();	
+			}
+		} catch(Exception e) {};
+		
+		bw2.close();
+		bf2.close();
+		
+	}
+		
+	
+	
 	public static void updateUser() throws IOException {
+		
 		System.out.println("Write Student's ID");
 		String ID2;
 		Scanner in = new Scanner(System.in);
 		ID2=in.nextLine();
+
 				
-		BufferedReader bf1 = new BufferedReader(new FileReader("C:\\OOPGit\\OOP\\Project2output.txt"));
+		BufferedReader bf1 = new BufferedReader(new FileReader("C:\\OOPGit\\OOP\\Project2\\output.txt"));
 		BufferedWriter bw1 = new BufferedWriter(new FileWriter("C:\\OOPGit\\OOP\\Project2\\output.txt"));
 		
 		for (int i=0; i<userDataList.size(); i++) {
-			if (userDataList.get(i).equals(ID2)) {
-				System.out.println("What do you want to change?");
-				Scanner sc= new Scanner(System.in);
+			if (userDataList.get(i).id.equals(ID2)) {
+				System.out.println("What do you want to change? (name, surname, password, id");
 				String updateField=in.nextLine();
-				try {
+			
 				switch(updateField) {
 				
 				case "name":
-					
+					System.out.println("Write new name");
 					updateField=in.nextLine();
 					userDataList.get(i).name= updateField;
-					bw1.write(userDataList.get(i).name);
+					WritetoFile();
 					break;
 					
 				case "surname":
-					
+					System.out.println("Write new surname");
 					updateField=in.nextLine();
 					userDataList.get(i).surname= updateField;
+					WritetoFile();
 					break;
 					
 				
 				case "password":
-					
+					System.out.println("Write new password");
 					updateField=in.nextLine();
 					userDataList.get(i).password= updateField;
+					WritetoFile();
 					break;
 					
-					
+				case "id":
+					System.out.println("Write new id");
+					updateField=in.nextLine();
+					userDataList.get(i).password= updateField;
+					WritetoFile();
+					break;	
 				}
-			}catch(IOException e) {}
-		}
+			}	
 		
 		} 
-		
 		bw1.close();
 		bf1.close();
 	}
