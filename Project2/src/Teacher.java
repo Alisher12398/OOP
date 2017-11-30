64c35000519e8cec47dbfd28be050e472d77127f
Yerkebulan <erke.zhumagali@gmail.com>
2017-12-01 05:48:11 +0600

    editing...

--- Project2/src/Teacher.java
+++ Project2/src/Teacher.java
@@ -1,9 +1,19 @@
+import java.io.Serializable;
 import java.lang.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
-public class Teacher extends Employee {
-	Title title;
+public class Teacher extends Employee implements Serializable {
+	public enum Degree{PhD, MASTER};
+	private Degree degree = Degree.MASTER;
+	Vector<Course> courses = new Vector<Course>();
+	public Teacher() {}
+	public Teacher(User teach) {
+		
+	}
+	
+	
 	
 	public void viewCourse() {
 		for (int i=0; i<DB.courses.size(); i++) {
