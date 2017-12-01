a22f63ec2a6580ed98c208234a8f32b173573964
Yerkebulan <erke.zhumagali@gmail.com>
2017-12-01 12:11:36 +0600

    Editing

--- Project2/src/Employee.java
+++ Project2/src/Employee.java
@@ -1,8 +1,13 @@
-/**
- */
+import java.
+
 public class Employee extends User {
 	
     private int salary;
+    
+    public Employee(String login, String password, String name, double salary) {
+        super(login, password, name);
+        this.salary = salary;
+    }
 
     private int yearofStartWork;
 
