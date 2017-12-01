3ce34bc8e44e0a5a63f9afad80178d549ad2dd69
Yerkebulan <erke.zhumagali@gmail.com>
2017-12-01 06:02:25 +0600

    editin'..

--- Project2/src/Course.java
+++ Project2/src/Course.java
@@ -1,41 +1,57 @@
 import java.util.*;
 
 public class Course {
-	private Teacher teacher;
-	private Student[] students;
-
+	private String courseName, code, courseFiles;
+	private int credits = 3;
 	
-	public Course(int maxStudents, Teacher teacher) {
-		students = new Student[maxStudents];
-		this.teacher = teacher;		
+	Teacher teacher = new Teacher();
+	public String getCourseName() {
+		return courseName;
 	}
 	
-	public void addStudent(Student student) {
-		for(int i = 0; i < students.length; i++) {
-			if(student == students[i]) 
-				continue;
-			
-			if(students[i] == null) {
-				students[i] = student;
-				return;
-			}
-		}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+		Random generator = new Random();
+		int i = generator.nextInt(900) + 100;
+		this.code = courseName + Integer.toString(i);
 	}
 	
-	public Teacher getTeacher() {
-		return teacher;
+	public String getCode() {
+		return code;
+	}
+	public int getCredits() {
+		return credits;
 	}
 	
-	public Student[] getStudents() {
-		return Arrays.copyOf(students, students.length);
+	public void setCredits(int credits) {
+		this.credits = credits;
 	}
 	
-	public boolean isFull() {
-		boolean full = true;
-		
-		for(Student student: students) 
-			full = student != null;
-		
-		return full;
+	public String getCourseFiles() {
+		return courseFiles;
+	}
+	
+	public void setCourseFiles(String courseFiles) {
+		this.courseFiles = courseFiles;
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
+	
+	public String toString() {
+		return "Course [courseName=" + courseName + "]";
 	}
 } 
